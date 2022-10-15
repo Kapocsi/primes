1ba807da7bd717c7d20e0608b55e8bc248037c9b
Thomas Kapocsi <tomkap011@gmail.com>
2022-10-14 19:32:48 -0600

    added function for generating progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn get_progress_bar(current: i32, max: i32, progress_bar_len: i32) -> String {
+    let bar_size = progress_bar_len as f32 / max as f32;
+    format!(
+        "|{}{}| {}/{} ~ {:.2}%",
+        { "█".repeat((current as f32 * bar_size) as usize) },
+        { "-".repeat(((max - current) as f32 * bar_size) as usize) },
+        current,
+        max,
+        (current as f32 / max as f32) * 100.00
+    )
+}
+
 fn main() {
     println!("Hello, world!");
 }
